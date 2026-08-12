3ea7104b0e8cc2dda2beb6d6953206f0a53e4d2e
mikepassal <mikepassalacqua@gmail.com>
2026-08-12 17:56:16 -0400

    Other realigners

---
+++ Cluster_analysis_files/Realign_fastqs_cellranger/realign_heat_samples.sh
@@ -0,0 +1,54 @@
+#!/bin/bash
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=1
+#SBATCH --cpus-per-task=24
+#SBATCH --array=1-3
+#SBATCH --time=10:00:00
+#SBATCH --mem=80GB
+#SBATCH --job-name=soybean_realign_heat
+#SBATCH --mail-type=FAIL
+#SBATCH --mail-user=michael.p@nyu.edu
+#SBATCH --account=torch_pr_121_general
+#SBATCH --output=/home/mp7563/Jobs_logs/Soybean_jobs/%A_%a_soybean_realign_heat.out
+#SBATCH --error=/home/mp7563/Jobs_logs/Soybean_jobs/%A_%a_soybean_realign_heat.err
+
+stdbuf -o0 echo "Script Started"
+
+SAMPLE_LABELS=(
+  "RNA_leaf_H1"
+  "RNA-Leaf-H2"
+  "RNa-leaf-heat_3"
+)
+
+RUN_IDS=(
+  "Heat_1"
+  "Heat_2"
+  "Heat_3"
+)
+
+FASTQ_LIST=(
+  "/projects/rps/cgsb/bergelson/bergelson-lab/Michael_P/Collaborator_data/Raw_fastq_files/GR00_Rep1/Leaf_heat1/RNA_LeafH1"
+  "/projects/rps/cgsb/bergelson/bergelson-lab/Michael_P/Collaborator_data/Raw_fastq_files/Rep_2/GR0073_RNA_resequenced_20251201_/GR0073/non_trimmed"
+  "/projects/rps/cgsb/bergelson/bergelson-lab/Michael_P/Collaborator_data/Raw_fastq_files/Rep_3/GR0192_RNA/RNA_fastq_raw"
+)
+
+IDX=$((SLURM_ARRAY_TASK_ID-1))
+RUN_ID="${RUN_IDS[$IDX]}"
+SAMPLE_PREFIX="${SAMPLE_LABELS[$IDX]}"
+FASTQS="${FASTQ_LIST[$IDX]}"
+
+mkdir -p /projects/rps/cgsb/bergelson/bergelson-lab/Michael_P/Soybean_results/realigned_sc/heat
+cd /projects/rps/cgsb/bergelson/bergelson-lab/Michael_P/Soybean_results/realigned_sc/heat || exit 1
+
+singularity exec --nv \
+    --overlay /home/mp7563/Python_envs/Torch_miniforge_env.ext3:ro /home/mp7563/Python_envs/ubuntu_cuda_image.sif \
+    /bin/bash -c "source /ext3/env.sh; conda activate Luke_terrace; \
+    cellranger count \
+    --id="$RUN_ID" \
+    --transcriptome=/projects/rps/cgsb/bergelson/bergelson-lab/Michael_P/Collaborator_data/Genomes/soy_Williams82_v3_with_organelles \
+    --fastqs="$FASTQS" \
+    --sample="$SAMPLE_PREFIX" \
+    --chemistry=ARC-v1 \
+    --localcores=24 \
+    --create-bam=true \
+    --localmem=75"
